2a35b8d860b1abd198bd34a99f772b1bdbb353f4
Levente26 <madi.levente26@gmail.com>
2022-05-04 21:58:56 +0200

    baszdmeg ez kurva sok volt egyszerre

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="increment">Increment</button>
+  <button @click="increase({value: 11})">Increase</button>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  methods: {
+    // addOne() {
+    //   this.$store.dispatch('increment');
+    // }
+    // ...mapActions(["increment", "increase"])
+    ...mapActions({
+      inc: "increment",
+      increase: "increase",
+    })
+  }
+}
+</script>
